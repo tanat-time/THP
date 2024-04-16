0d792c2e1bc86ac3f9d79d29dbc9b0d736289415
tanat-time <146315516+tanat-time@users.noreply.github.com>
2024-04-16 19:43:48 +0700

    Add files via upload

---
+++ memory.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import docx
+from fnreadfilename import readfilesname
+path = '10Mar'
+x = readfilesname(path)
+print(x)
+a=0
+df = pd.DataFrame(columns=['DB Name', 'Use','Total'])
+while a<(len(x)):
+    df1 = pd.read_csv(path+'\\'+x[a],header=None)
+    df1 = df1.rename(columns={0: 'DB Name',1:'Use',2:'Total'})
+    df1.iloc[0,0]= x[a]
+    frames = [df,df1]
+    df = pd.concat(frames)
+    a=a+1
+print(df)
